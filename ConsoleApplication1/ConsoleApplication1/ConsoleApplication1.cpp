4201afadfc87c7084692583241daa0f90b8ed878
linhGithub <hoangsylinh31012003@gmail.com>
2022-03-22 22:03:32 +0700

    thuchanh commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+class SoPhuc {
+private:
+	int PhanThuc, PhanAo;
+public:
+	void nhap() {
+		cout << " nhap vao phan thuc: ";
+		cin >> PhanThuc;
+		cout << " nhap vao phan ao: ";
+		cin >> PhanAo;
+	}
+	void in() {
+		cout << "\nA = " << PhanThuc << " + " << PhanAo << " *j" << endl;
+	}
+	float getPA() {
+		return PhanAo;
+	}
+	float getPT() {
+		return PhanThuc;
+	}
+};
+int main()
+{
+	SoPhuc a[2];
+	for (int i = 0; i < 2; i++)
+	{
+		a[i].nhap();
+	}
+
+	cout << "\n Tong cua 2 so thuc vhua nhap la: " << a[0].getPT() + a[1].getPT() << " + " << a[0].getPA() + a[1].getPA() << " *j" << endl;
+	cout << "\n Tong cua 2 so thuc vhua nhap la: " << a[0].getPT() - a[1].getPT() << " + " << a[0].getPA() - a[1].getPA() << " *j" << endl;
+	cout << "\n Tong cua 2 so thuc vhua nhap la: " << a[0].getPT() / a[1].getPT() << " + " << a[0].getPA() / a[1].getPA() << " *j" << endl;
+	cout << "\n Tong cua 2 so thuc vhua nhap la: " << a[0].getPT() * a[1].getPT() << " + " << a[0].getPA() * a[1].getPA() << " *j" << endl;
+	return 0;
+}
